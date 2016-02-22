f8db7a82731b4b1e3820066e0ccc447c73e9835d
abhi195 <abhisapariya@gmail.com>
2016-02-23 02:11:27 +0530

    Completed Lesson-5 Mini-Project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,28 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# <--No of Features and size of enron dataset--> #
+# print len(enron_data['METTS MARK'])
 
+# <--No of poi's in enron dataset--> #
+# poino=0
+# for i in enron_data:
+# 	if enron_data[i]['poi']==True:
+# 		poino+=1
+# print poino
+
+noPayments=0
+pois=0
+for i in enron_data:
+	if enron_data[i]['poi']==True:
+		pois+=1
+		if enron_data[i]['total_payments']=='NaN':
+			noPayments+=1
+print noPayments
+print pois
+
+# print enron_data["LAY KENNETH L"]["total_payments"]
+
+# print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+# print enron_data["SKILLING JEFFREY K"]["total_payments"]
