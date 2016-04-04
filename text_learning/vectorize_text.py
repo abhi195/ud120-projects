18263ba70eece901071d7418e8d041a433561f4e
abhi195 <abhisapariya@gmail.com>
2016-04-05 02:23:40 +0530

    Completed Lesson-10 Mini-Project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,20 +41,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        #if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            signature_words = ["sara", "shackleton", "chris", "germani"]
+            for i in signature_words:
+                text=text.replace(i,"")
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name=='sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
 
             email.close()
@@ -63,13 +72,23 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 
+print "word_data[152]:",word_data[152]
+
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
+# from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# sw = stopwords.words("english")
 
+# for line in word_data:
+#     for i in sw:
+#         line.replace(i,"")
+
+vectorizer = TfidfVectorizer(stop_words='english')
+tfidf = vectorizer.fit_transform(word_data)
+print "Number of unique words: ",len(vectorizer.get_feature_names())
+print "Word number 34597: ",vectorizer.get_feature_names()[34597]
